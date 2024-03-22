6196f1153f5eb3127c2c9c2080b6d4b5a48eb5a0
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2024-03-22 22:11:49 +0100

    project bootstrap

---
+++ src/main/java/com/emekauk/paymentgatewaymiddleware/PaymentGatewayMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.emekauk.paymentgatewaymiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayMiddlewareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emekauk/paymentgatewaymiddleware/PaymentGatewayMiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emekauk.paymentgatewaymiddleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayMiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
